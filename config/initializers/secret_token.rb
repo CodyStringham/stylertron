a8b7eca71b1291502d99e2b7b9d3b2643cc40ba2
codystringham <cs.negativ@gmail.com>
2014-02-28 21:35:31 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Stylertron::Application.config.secret_token = '962ac1b028f7bd598e70375c52e01b0e50d733697a65160b4213e6975a2f894115f8f6970cffacb8d4611c6b07b573d1fe1570fcb397cfe937c9b23d3e969396'
